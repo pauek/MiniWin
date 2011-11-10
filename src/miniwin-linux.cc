bddde2ab6331543b1c5df5e1e3f9c29148a3de3f
Pau Fernández <pau.fernandez@upc.edu>
2011-11-10 11:01:10 +0100

    Gestión del lock por mi cuenta, pasando de XInitThreads...

--- src/miniwin-linux.cc
+++ src/miniwin-linux.cc
@@ -26,6 +26,7 @@ Pixmap          _buffer;
 std::queue<int> _teclas;
 bool            _end = false;
 pthread_t       _thread;
+pthread_mutex_t _mutex = PTHREAD_MUTEX_INITIALIZER;
 
 //////////////////////////////////////////////////////////////////////
 
@@ -61,10 +62,13 @@ inline void _fill() {
 }
 
 inline void _refresh() {
-   XCopyArea(_dsp, _buffer, _win, _bufgc, 
+   XCopyArea(_dsp, _buffer, _win, _bufgc,
              0, 0, _width, _height, 0, 0);
    XFlush(_dsp);
 }
+
+inline void _lock()   { pthread_mutex_lock(&_mutex); }
+inline void _unlock() { pthread_mutex_unlock(&_mutex); }
 
 void _handlekey(KeySym key) {
    switch (key) {
@@ -97,21 +101,21 @@ void _change_width_height(int w, int h) {
 
 struct _mouse_state_t {
    float x, y;
-   bool in_window, left_button, right_button;   
+   bool in_window, left_button, right_button;
 } _mouse_state;
 
 void _open_display() {
    // "XInitThreads must be the first Xlib function a multi-threaded program calls"
    // http://tronche.com/gui/x/xlib/display/XInitThreads.html
-   XInitThreads(); 
-	_dsp = XOpenDisplay(NULL);   
+   /* XInitThreads(); */
+	_dsp = XOpenDisplay(NULL);
 }
 
 void _new_window() {
-	_win = XCreateSimpleWindow(_dsp, RootWindow(_dsp, 0), 
-                              0, 0, _width, _height, 
+	_win = XCreateSimpleWindow(_dsp, RootWindow(_dsp, 0),
+                              0, 0, _width, _height,
                               0, BlackPixel (_dsp, 0), BlackPixel(_dsp, 0));
-	XSelectInput(_dsp, _win, 
+	XSelectInput(_dsp, _win,
                 StructureNotifyMask |
                 ExposureMask |
                 KeyPressMask |
@@ -143,7 +147,7 @@ void _new_buffer(bool free = false) {
    XGetWindowAttributes(_dsp, _win, &attrs);
    _buffer = XCreatePixmap(_dsp, _win, _width, _height, attrs.depth);
 	_bufgc = XCreateGC(_dsp, _buffer, 0, 0);
-   _color(miniwin::NEGRO); 
+   _color(miniwin::NEGRO);
    _fill();
    _color(miniwin::BLANCO);
 }
@@ -161,58 +165,66 @@ void _maybe_call_main() {
    }
 }
 
+void _process_event() {
+   switch  (_report.type) {
+   case Expose: {
+      _refresh();
+      break;
+   }
+   case KeyPress: {
+      KeySym key = XLookupKeysym(&_report.xkey, 0);
+      _handlekey(key);
+      break;
+   }
+   case MapNotify: {
+      _maybe_call_main();
+      break;
+   }
+   case ClientMessage: {
+      // WM_DELETE_WINDOW
+      _end = true;
+      break;
+   }
+   case MotionNotify: {
+      _mouse_state.x = _report.xmotion.x;
+      _mouse_state.y = _report.xmotion.y;
+      break;
+   }
+   case ButtonPress: {
+      switch (_report.xbutton.button) {
+      case 1: _mouse_state.left_button  = true; break;
+      case 3: _mouse_state.right_button = true; break;
+      }
+      break;
+   }
+   case ButtonRelease: {
+      switch (_report.xbutton.button) {
+      case 1: _mouse_state.left_button = false; break;
+      case 3: _mouse_state.right_button = false; break;
+      }
+      break;
+   }
+   case EnterNotify:
+   case LeaveNotify: {
+      _mouse_state.in_window = (_report.type == EnterNotify);
+      break;
+   }
+   }
+}
+
 int main() {
    _open_display();
    _new_window();
    _new_buffer();
 
 	while (!_end)  {
-      XNextEvent(_dsp, &_report); // this blocks
-
-		switch  (_report.type) {
-      case Expose: {
-         _refresh();
-			break;
-      }
-      case KeyPress: {
-         KeySym key = XLookupKeysym(&_report.xkey, 0);
-         _handlekey(key);
-			break;
-      }
-      case MapNotify: {
-         _maybe_call_main();
-         break;
-      }
-      case ClientMessage: {
-         // WM_DELETE_WINDOW
-         _end = true;
-         break;
-      }
-      case MotionNotify: {
-         _mouse_state.x = _report.xmotion.x;
-         _mouse_state.y = _report.xmotion.y;
-         break;
-      }
-      case ButtonPress: {
-         switch (_report.xbutton.button) {
-         case 1: _mouse_state.left_button  = true; break;
-         case 3: _mouse_state.right_button = true; break;
-         }
-         break;
-      }
-      case ButtonRelease: {
-         switch (_report.xbutton.button) {
-         case 1: _mouse_state.left_button = false; break;
-         case 3: _mouse_state.right_button = false; break;
-         }
-         break;
-      }
-      case EnterNotify:
-      case LeaveNotify: {
-         _mouse_state.in_window = (_report.type == EnterNotify);
-         break;
-      }
-		}
+       _lock();
+       if (XPending(_dsp) > 0) {
+          XNextEvent(_dsp, &_report); // this blocks
+          _process_event();
+       }
+       _unlock();
+       usleep(100);
 	}
    pthread_cancel(_thread);
    XDestroyWindow(_dsp, _win);
@@ -230,10 +242,14 @@ int main() {
 namespace miniwin {
 
 void vredimensiona(int ancho, int alto) {
+   _lock();
    _change_width_height(ancho, alto);
+   _unlock();
    _width  = ancho;
    _height = alto;
+   _lock();
    _new_buffer(true);
+   _unlock();
 }
 
 int vancho() {
@@ -245,59 +261,88 @@ int valto() {
 }
 
 void vcierra() {
+   _lock();
    _end = true;
    XUnmapWindow(_dsp, _win);
    XFlush(_dsp);
+   _unlock();
 }
 
 void punto(float x, float y) {
+   _lock();
    XDrawPoint(_dsp, _buffer, _bufgc, x, y);
+   _unlock();
 }
 
 void linea(float x_ini, float y_ini, float x_fin, float y_fin) {
+   // cerr << "linea(" << x_ini << ", " << y_ini << ", " << x_fin << ", " << y_fin << ")" << endl;
+   _lock();
    XDrawLine(_dsp, _buffer, _bufgc, x_ini, y_ini, x_fin, y_fin);
+   _unlock();
 }
 
 void rectangulo(float izq, float arr, float der, float aba) {
-	XDrawRectangle(_dsp, _buffer, _bufgc, izq, arr, der - izq, aba - arr);
+   _lock();
+   XDrawRectangle(_dsp, _buffer, _bufgc, izq, arr, der - izq, aba - arr);
+   _unlock();
 }
 
 void rectangulo_lleno(float izq, float arr, float der, float aba) {
-	XFillRectangle(_dsp, _buffer, _bufgc, izq, arr, der - izq, aba - arr);
+   _lock();
+   XFillRectangle(_dsp, _buffer, _bufgc, izq, arr, der - izq, aba - arr);
+   _unlock();
 }
 
 void circulo(float x_cen, float y_cen, float radio) {
    float x = x_cen - radio, y = y_cen - radio;
+   _lock();
    XDrawArc(_dsp, _buffer, _bufgc, x, y, 2*radio, 2*radio, 0, 360 * 64);
+   _unlock();
 }
 
 void circulo_lleno(float x_cen, float y_cen, float radio) {
    float x = x_cen - radio, y = y_cen - radio;
+   _lock();
    XFillArc(_dsp, _buffer, _bufgc, x, y, 2*radio, 2*radio, 0, 360 * 64);
+   _unlock();
 }
 
 void texto(float x, float y, const std::string& texto) {
+   _lock();
    XDrawString(_dsp, _buffer, _bufgc, x, y, texto.c_str(), texto.size());
+   _unlock();
 }
 
 void color_rgb(int r, int g, int b) {
+   _lock();
    _color_rgb(r, g, b);
+   _unlock();
 }
 
 void color(int col) {
+   _lock();
    _color(col);
+   _unlock();
 }
 
 
 void refresca() {
-   XClearArea(_dsp, _win, 0, 0, 0, 0, True);
+   XEvent event;
+   event.type = Expose;
+   event.xexpose.window = _win;
+   // repintar sin borrar (evitar XClearArea...)
+   _lock();
+   XSendEvent(_dsp, _win, False, ExposureMask, &event);
    XFlush(_dsp);
+   _unlock();
 }
 
 void borra() {
    int prev = _color(miniwin::NEGRO);
+   _lock();
    _fill();
    _color(prev);
+   _unlock();
 }
 
 int tecla() {
@@ -305,7 +350,9 @@ int tecla() {
       return NINGUNA;
    } else {
       int t = _teclas.front();
+      _lock();
       _teclas.pop();
+      _unlock();
       return t;
    }
 }
